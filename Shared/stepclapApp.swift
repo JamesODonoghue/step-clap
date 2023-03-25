9b27241360345f235c32f160d8f9c2102f785d85
James O'Donoghue <jameso@James-ODonoghue-Macbook-Pro-2.local>
2023-03-25 11:30:53 -0700

    chore: initial commit

---
+++ Shared/stepclapApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepclapApp.swift
+//  Shared
+//
+//  Created by James O'Donoghue on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct stepclapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
